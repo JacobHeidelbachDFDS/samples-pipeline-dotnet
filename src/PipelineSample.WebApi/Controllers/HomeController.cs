2371b8fedf3d55cf76e4598caa45733e315d425a
Thomas Fischer <thfis@dfds.com>
2018-06-18 13:44:16 +0200

    add Pipeline.WebApi project and solution

---
+++ src/PipelineSample.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PipelineSample.WebApi.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Api");
+        }
+    }
+}
